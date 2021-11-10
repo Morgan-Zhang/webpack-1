b407203cf8b5c208c023992367ffbf3b5bdf4a66
morgan <zhangteng_yx@126.com>
2021-11-10 20:53:58 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const path = require("path");
+const { title } = require("process");
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].[contenthash].js",
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "morgan",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
